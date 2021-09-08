1905fa62596fe0433327367f28edd391ec454572
dan-reid <dan.j.reid89@gmail.com>
2021-09-08 23:40:55 +0100

    allow user to configure ratio

--- nscale/main.c
+++ nscale/main.c
@@ -10,13 +10,21 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
+    if (argc < 3) {
         printf("usage: nscale notes midinote\n");
         return 1;
     }
     
     int notes = atoi(argv[1]);
     int midinote = atoi(argv[2]);
+    
+    double interval;
+    char *ptr;
+    if (argc < 4) {
+        interval = 2.0;
+    } else {
+        interval = strtod(argv[3], &ptr);
+    }
     
     if (notes < 1) {
         printf("Error: notes must be positive\n");
@@ -38,7 +46,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     
-    double ratio = pow(2.0, 1.0 / 12.0);
+    double ratio = pow(interval, 1.0 / 12.0);
     double c5 = 220.0 * pow(ratio, 3);
     double c0 = c5 * pow(0.5, 5);
     double frequency = c0 * pow(ratio, midinote);
