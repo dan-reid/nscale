06fe200649ca226cf325ec59af1c1c43400a9d67
dan-reid <dan.j.reid89@gmail.com>
2021-09-08 23:16:16 +0100

    Initial Commit

---
+++ nscale/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  nscale
+//
+//  Created by Daniel Reid on 08/09/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
